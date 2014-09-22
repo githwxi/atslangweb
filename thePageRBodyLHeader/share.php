1ba1434d56a4f8dda51bb75b34809d104d263b61
Hongwei Xi <gmhwxi@gmail.com>
2014-09-22 18:55:47 -0400

    Updating: improving ats2langweb

--- thePageRBodyLHeader/share.php
+++ thePageRBodyLHeader/share.php
@@ -17,7 +17,7 @@ thePageRBodyLHeader_Downloads()
 echo '<ul>'; echo "\n";
 echo '<li><a href="#ATS_packages">ATS packages for download</a></li>'; echo "\n";
 echo '<li><a href="#Requirements_install">Requirements for installation</a></li>'; echo "\n";
-echo '<li><a href="#Installation_precompile">Installation with a precompiled package</a></li>'; echo "\n";
+echo '<li><a href="#Installation_precompile">Installation with precompiled code</a></li>'; echo "\n";
 echo '<li><a href="#Installation_sourcecomp">Installation through source compilation</a></li>'; echo "\n";
 echo '<li><a href="#Installation_ATS2_contrib">Installation of ATS2-contrib</a></li>'; echo "\n";
 echo '</ul>'; echo "\n";
