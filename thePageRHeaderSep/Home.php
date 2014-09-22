e85a630c806b3119301a55f3313b7194f4fcb0b0
Hongwei Xi <gmhwxi@gmail.com>
2014-09-22 16:45:21 -0400

    Updating: improving ats2langweb

--- thePageRHeaderSep/Home.php
+++ thePageRHeaderSep/Home.php
@@ -1,11 +1,11 @@
 <style>
+#thePageRHeaderSep
+{
+  color: #FFFFF0;
+}
 #thePageRHeaderSepL
 {
   text-align: left;
-}
-#thePageRHeaderSepR
-{
-  text-align: right;
 }
 #thePageRHeaderSepL ul
 {
@@ -17,26 +17,62 @@
 {
   display: inline;
 }
+#thePageRHeaderSepR
+{
+}
+#thePageRHeaderSepR_input
+{
+  height: 100%;
+  font-size: 12pt;
+  border-radius: 6px;
+  background: #FFFFF0 url(//www.google.com/coop/intl/en/images/google_custom_search_watermark.gif) left no-repeat;
+}
 </style>
 
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 <tr>
 
-<td style="vertical-align: top; width: 80%;">
+<td width="75%">
 <div id="thePageRHeaderSepL">
 <ul>
-<li><a href="./Home.php">Home</a></li>
-<li><a href="./Downloads.php">Downloads</a></li>
-<li><a href="./Documents.php">Documents</a></li>
-<li><a href="./Libraries.php">Libraries</a></li>
-<li><a href="./Communities.php">Communities</a></li>
+<li>Home</li>
+<li>Downloads</li>
+<li>Documents</li>
+<li>Libraries</li>
+<li>Communities</li>
 </ul>
 </div>
 </td>
 
-<td style="vertical-align: top; width: 20%;">
-<div id="thePageRHeaderSepR">
-SearchButton
+<td width="25%">
+<div
+id="thePageRHeaderSepR"
+style="margin-top:6px;margin-bottom:6px;"
+>
+<form
+id="search-form"
+action="https://www.google.com/cse"
+>
+<table>
+<tr>
+<td>
+<input
+id="thePageRHeaderSepR_input"
+type="text" name="q" size="27"
+onblur="if (/^\s*$/.test(this.value)) this.style.background='#FFFFF0 url(//www.google.com/coop/intl/en/images/google_custom_search_watermark.gif) left no-repeat'"
+onfocus="this.style.background='#FFFFF0'"
+/>
+</td>
+<td>
+<input type="hidden" name="ie" value="UTF-8" />
+<input type="hidden" name="cx" value="007881927213964201908:gqahxj364ly" />
+<!--
+<input class="button" type="submit" name="sa" value="Search" />
+-->
+</td>
+</tr>
+</table>
+</form>
 </div>
 </td>
 
