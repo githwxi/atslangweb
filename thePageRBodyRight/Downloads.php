9fd684c553df4853c86636afec129fd311d6f221
Hongwei Xi <gmhwxi@gmail.com>
2014-09-25 01:20:12 -0400

    Updating: improving ats2langweb

--- thePageRBodyRight/Downloads.php
+++ thePageRBodyRight/Downloads.php
@@ -12,7 +12,23 @@
 style="margin-top:8px;padding-top:8px;padding-left:4px;padding-right:4px"
 ><!--div-->
 
-<h2 style="display:inline;">More on packages of ATS2</h2>
+<h2 style="display:inline;">More on ATS packages</h2>
+
+<hr></hr>
+
+<p>
+ATS/Postiats (ATS2) is implemented in ATS/Anairiats (ATS1).
+The lastest released version of ATS1 is
+<a href="http://sourceforge.net/projects/ats-lang/files/latest/download?source=files">ATS1-0.2.11</a>,
+which we need in order to compile ATS2.
+ATS1 can be bootstrapped, that is, it is capable of compiling itself.
+</p>
+
+<p>
+Note that only C code generated from compiling ATS1 source is included in a
+released package of ATS2. Such a package can then be readily compiled into
+object code by a C compiler such as gcc and clang.
+</p>
 
 <hr></hr>
 
