f770bc9e3c1035f255dd8890160bc15386a0f8d7
Hongwei Xi <gmhwxi@gmail.com>
2014-09-25 01:37:57 -0400

    Updating: improving ats2langweb

--- thePageRBodyRight/Downloads.php
+++ thePageRBodyRight/Downloads.php
@@ -22,20 +22,24 @@ The lastest released version of ATS1 is
 <a href="http://sourceforge.net/projects/ats-lang/files/latest/download?source=files">ATS1-0.2.11</a>,
 which we need in order to compile ATS2.
 ATS1 can be bootstrapped, that is, it is capable of compiling itself.
+A simple shell script for installing ATS1 under Linux can be found
+<a href="https://github.com/githwxi/ATS-Postiats/blob/master/travis-ci/setup.sh">on-line</a>.
+It is clear from the script that installing ATS1 is essentially the same as installing ATS2.
 </p>
+
+<hr></hr>
 
 <p>
 Note that only C code generated from compiling ATS1 source is included in a
 released package of ATS2. Such a package can then be readily compiled into
-object code by a C compiler such as gcc and clang. In general, it is fairly
+object code by a C compiler such as gcc and clang. In general, it is largely
 straightforward to port ATS2 to a platform running a Unix-like OS.
 </p>
 
 <hr></hr>
 
 <p>
-A simple shell script for installing ATS1 under Linux can be found
-<a href="https://github.com/githwxi/ATS-Postiats/blob/master/travis-ci/setup.sh">on-line</a>.
+ATS2-github is for people interested in developing ATS2.
 </p>
 
 <hr></hr>
