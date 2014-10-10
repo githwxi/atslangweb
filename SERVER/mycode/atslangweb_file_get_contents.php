529e7c3d28c00d16b038844cb266e10bc7ebb12e
Hongwei Xi <gmhwxi@gmail.com>
2014-10-09 21:45:12 -0400

    Updating: improving ats2langweb/patsoptaas

--- SERVER/mycode/atslangweb_file_get_contents.php
+++ SERVER/mycode/atslangweb_file_get_contents.php
@@ -4,6 +4,10 @@ $fname = $_REQUEST["fname"];
 /*
 $fname = "http://www.ats-lang.org";
 */
+if(!parse_url($fname))
+{
+  $fname = "http://www.ats-lang.org/$fname";
+}
 $contents = file_get_contents($fname);
 if($contents)
 {
