e4057a946fa5349300f55f8c7e932006260b707c
Hongwei Xi <gmhwxi@gmail.com>
2015-09-03 17:21:34 -0400

    Updating: improving libatscc2js_print_store.js

--- CLIENT/MYCODE/libatscc2js_print_store.js
+++ CLIENT/MYCODE/libatscc2js_print_store.js
@@ -46,6 +46,24 @@ ats2jspre_print_int(x)
 /* ****** ****** */
 
 function
+ats2jspre_print_uint(x)
+{
+  ats2jspre_the_print_store.push(String(x));
+  return;
+}
+
+/* ****** ****** */
+
+function
+ats2jspre_print_bool(x)
+{
+  ats2jspre_the_print_store.push(String(x));
+  return;
+}
+
+/* ****** ****** */
+
+function
 ats2jspre_print_double(x)
 {
   ats2jspre_the_print_store.push(String(x));
